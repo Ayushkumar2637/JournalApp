b53566328c69c9b36cc068e91df3d22d674ec8d4
Ayush <ayushkumar2637@gmail.com>
2026-03-02 21:23:59 +0530

    Feat : Profiles are implemented

--- src/main/java/com/ayushkumar/journalApp/Config/SecurityConfig.java
+++ src/main/java/com/ayushkumar/journalApp/Config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.ayushkumar.journalApp.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -10,6 +11,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+//@Profile("prod")
 public class SecurityConfig {
 
     @Bean
